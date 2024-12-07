1b9f5b845f45cd2b3d408ccc84e426730bef0b34
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-07 01:26:26 +0100

    Create RedisClient class

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client
+      .on('error', (err) => console.log(err.message));
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const promise = getAsync(key);
+    return promise;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
