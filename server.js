b7d8c59791191c62f206c0cbbd4e8d0c61d15467
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-10 14:59:36 +0100

    Add express.json() middleware

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import router from './routes/index';
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Respond to request
-app.use(router);
+// Middleware to pass JSON, Respond to request
+app.use(express.json(), router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
