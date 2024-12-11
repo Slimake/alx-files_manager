cdaba0454e73208c16862d96626acfd3407a8d2f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-11 21:34:09 +0100

    Add limit to express.json()

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware to pass JSON, Respond to request
-app.use(express.json(), router);
+app.use(express.json({ limit: '5mb' }), router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
